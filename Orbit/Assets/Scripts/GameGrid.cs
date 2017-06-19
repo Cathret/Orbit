95b7c5d970dd49ede6484fa3eb49fb2f513cfaad
niokki <n.fontes@student.isartdigital.com>
2017-06-19 10:45:48 +0200

    Add Grid

---
+++ Orbit/Assets/Scripts/GameGrid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    [SerializeField]
+    private uint m_width;
+    [SerializeField]
+    private uint m_height;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
