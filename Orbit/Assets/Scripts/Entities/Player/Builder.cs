a78aab173cec15760c39e5c31a696df91dd47356
Guillaume <gnewton.geek@gmail.com>
2017-06-25 19:29:08 +0200

    Changed prefab and added _particlesOnBuild

--- Orbit/Assets/Scripts/Entities/Player/Builder.cs
+++ Orbit/Assets/Scripts/Entities/Player/Builder.cs
@@ -41,12 +41,18 @@ namespace Orbit.Entity
             protected set { _canBuildResource = value; }
         }
         private bool _canBuildResource;
+
+        [SerializeField]
+        private ParticleSystem _particlesOnBuild = null;
         #endregion
 
         #region Protected functions
         protected override void Awake()
         {
             base.Awake();
+
+            if ( _particlesOnBuild == null )
+                Debug.LogWarning( "Builder.Awake() - particles on build not set" );
 
             GameManager.Instance.OnAttackMode.AddListener( ChangeMode );
             GameManager.Instance.OnBuildMode.AddListener( ChangeMode );
@@ -91,6 +97,9 @@ namespace Orbit.Entity
 
         public void DropResources()
         {
+            if ( _particlesOnBuild != null )
+                _particlesOnBuild.Play();
+            
             GameManager.Instance.ResourcesCount += ResourcesToDrop;
             CanBuildResource = false;
         }
