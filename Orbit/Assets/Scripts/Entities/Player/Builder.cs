449ba18d50a973fb3bf2922d6a6236033b3274d8
Guillaume <gnewton.geek@gmail.com>
2017-06-25 19:21:27 +0200

    Implemented Builder
    Need to set icon and other things, but working as expected.

---
+++ Orbit/Assets/Scripts/Entities/Player/Builder.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+namespace Orbit.Entity
+{
+    public class Builder : AUnitController,
+                           IDropResources
+    {
+        #region Members
+        public uint ResourcesToDrop
+        {
+            get { return Power; }
+        }
+
+        public float RechargeSpeed
+        {
+            get { return _rechargeSpeed; }
+            protected set { _rechargeSpeed = value; }
+        }
+        [SerializeField]
+        private float _rechargeSpeed;
+
+        public float BuildTimer
+        {
+            get { return _buildTimer; }
+            protected set
+            {
+                _buildTimer = value;
+
+                if ( _buildTimer >= _rechargeSpeed )
+                {
+                    _buildTimer = 0.0f;
+                    _canBuildResource = true;
+                }
+            }
+        }
+        private float _buildTimer = 0;
+
+        public bool CanBuildResource
+        {
+            get { return _canBuildResource; }
+            protected set { _canBuildResource = value; }
+        }
+        private bool _canBuildResource;
+        #endregion
+
+        #region Protected functions
+        protected override void Awake()
+        {
+            base.Awake();
+
+            GameManager.Instance.OnAttackMode.AddListener( ChangeMode );
+            GameManager.Instance.OnBuildMode.AddListener( ChangeMode );
+        }
+
+        protected override void UpdateAttackMode()
+        {
+            base.UpdateAttackMode();
+
+            if ( CanBuildResource )
+                DropResources();
+            else
+                BuildTimer += Time.deltaTime;
+        }
+
+        protected override void OnDestroy()
+        {
+            if ( GameManager.Instance )
+            {
+                GameManager.Instance.OnAttackMode.RemoveListener( ChangeMode );
+                GameManager.Instance.OnBuildMode.RemoveListener( ChangeMode );
+            }
+            base.OnDestroy();
+        }
+        #endregion
+
+        public override void ExecuteOnClick( Vector3 target )
+        {
+            // Automatic, so do nothing
+        }
+
+        private void ChangeMode()
+        {
+            ResetCooldown();
+        }
+
+        private void ResetCooldown()
+        {
+            CanBuildResource = false;
+            BuildTimer = 0.0f;
+        }
+
+        public void DropResources()
+        {
+            GameManager.Instance.ResourcesCount += ResourcesToDrop;
+            CanBuildResource = false;
+        }
+    }
+}
