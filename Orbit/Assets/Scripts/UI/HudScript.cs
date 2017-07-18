a68923ea076c67209874c233140b7b61df671285
n.fontes <n.fontes@student.isartdigital.com>
2017-07-18 13:21:03 +0200

    Add warning show

--- Orbit/Assets/Scripts/UI/HudScript.cs
+++ Orbit/Assets/Scripts/UI/HudScript.cs
@@ -17,16 +17,31 @@ public class HudScript : MonoBehaviour
     [SerializeField]
     private Button _rotateIInvClockWise;
 
+    [SerializeField, Header("Warning")]
+    private Image[] _warningImages;
+    [SerializeField]
+    private float _warningShowTime = 0.3f;
+    [SerializeField]
+    private uint _warningShowRepetition = 3;
+
+    void Awake()
+    {
+        GameManager.Instance.OnResourcesChange += UpdateResourcesText;
+        UpdateResourcesText(GameManager.Instance.ResourcesCount);
+        WaveManager.Instance.RoundChanged += UpdateRoundText;
+
+        WaveManager.Instance.OnNewWave += ShowWarning;
+        for (int i = 0; i < _warningImages.Length; ++i)
+            _warningImages[i].gameObject.SetActive(false);
+    }
     // Use this for initialization
     void Start()
     {
-        GameManager.Instance.OnResourcesChange += UpdateResourcesText;
-        UpdateResourcesText( GameManager.Instance.ResourcesCount );
-        WaveManager.Instance.RoundChanged += UpdateRoundText;
         if ( _rotateClockWise )
             _rotateClockWise.onClick.AddListener( GameGrid.Instance.RotateClockwise );
+
         if ( _rotateIInvClockWise )
-            _rotateIInvClockWise.onClick.AddListener( GameGrid.Instance.RotateReverseClockwise );
+            _rotateIInvClockWise.onClick.AddListener( GameGrid.Instance.RotateReverseClockwise );     
     }
 
     // Update is called once per frame
@@ -50,5 +65,40 @@ public class HudScript : MonoBehaviour
     {
         if ( _zoneText )
             _zoneText.text = level.ToString();
+    }
+
+    void ShowWarning( GameCell.Quarter quarter )
+    {
+        int index;
+        switch ( quarter )
+        {
+            case GameCell.Quarter.TopRight:
+                index = 0;
+                break;
+            case GameCell.Quarter.BottomRight:
+                index = 1;
+                break;
+            case GameCell.Quarter.BottomLeft:
+                index = 2;
+                break;
+            case GameCell.Quarter.TopLeft:
+                index = 3;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException( "quarter", quarter, null );
+        }
+        StartCoroutine( ShowWarningCoroutine( index ) );
+    }
+
+    IEnumerator ShowWarningCoroutine( int index )
+    {
+        GameObject o = _warningImages[index].gameObject;
+        for ( int i = 0; i < _warningShowRepetition; ++i )
+        {
+            o.SetActive( true );
+            yield return new WaitForSeconds( _warningShowTime );
+            o.SetActive( false );
+            yield return new WaitForSeconds( _warningShowTime );
+        }
     }
 }
